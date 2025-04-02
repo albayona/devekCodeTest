4f51b533fa98e1effba467daca6a1018fda00c2d
Andres Bayona <andresbayona@Mac.attlocal.net>
2025-04-01 20:28:07 -0400

    Initial Commit

--- Backend/AuthHistoryProject/settings.py
+++
@@ -1,9 +0,0 @@
-from datetime import timedelta
-
-# Constants
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-# Database URL (SQLite)
-DATABASE_URL = "sqlite:///./chat_app.db"
